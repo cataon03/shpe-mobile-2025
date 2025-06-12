77f4ec2225c479ba7c1f1372d7f6d1c6d489da0c
do2523 <do262431@gmail.com>
2025-06-11 23:12:48 -0400

    Add Cards Component and Make Cards Functional

--- lib/landing.dart
+++ lib/landing.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:shpeucfmobile/event_card.dart';
 import 'package:shpeucfmobile/widgets/custom_button.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class Landing extends StatelessWidget {
   const Landing({super.key});
 
+  void showEventDialog(
+    BuildContext context,
+    String title,
+    String time,
+    String description,
+  ) {
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: Text(title),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Time: $time'),
+                const SizedBox(height: 8),
+                Text(description),
+              ],
+            ),
+            actions: [
+              TextButton(
+                child: const Text('Close'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
-
         children: [
           Image.asset('lib/images/background.png', fit: BoxFit.cover),
 
@@ -18,7 +48,7 @@ class Landing extends StatelessWidget {
             top: 75,
             left: 30,
             right: 30,
-            child: Align( 
+            child: Align(
               alignment: Alignment.topCenter,
               child: SvgPicture.asset('lib/images/SHPE_Logo.svg', width: 150),
             ),
@@ -30,11 +60,64 @@ class Landing extends StatelessWidget {
             right: 30,
             child: CustomButton(
               text: 'Profile Name',
-              backgroundColor: Color(0xFFF2AC02),
-              textColor: Color.fromARGB(255, 31, 62, 105),
+              backgroundColor: const Color(0xFFF2AC02),
+              textColor: const Color.fromARGB(255, 31, 62, 105),
               onPressed: () {
-                print('Clicked!');
+                print('Someone clicked yay!!!');
               },
+            ),
+          ),
+
+          Positioned(
+            top: 330,
+            left: 0,
+            right: 0,
+            child: SizedBox(
+              height: 160,
+              child: Center(
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(horizontal: 18),
+                  children: [
+                    EventCard(
+                      title: 'Reyjays workshop on Java',
+                      time: 'Today at 2pm',
+                      description: 'come leearn some java witth rayjay!!',
+                      onTap:
+                          () => showEventDialog(
+                            context,
+                            'Tech Talk',
+                            'Today at 4pm',
+                            'come learn some java witth reyjay!!',
+                          ),
+                    ),
+                    EventCard(
+                      title: 'Resume Workshop by daniel!!',
+                      time: 'june 15 at 4pm',
+                      description: 'get that resume in top shape!!!',
+                      onTap:
+                          () => showEventDialog(
+                            context,
+                            'Resume Workshop',
+                            'june 15 at 4pm',
+                            '',
+                          ),
+                    ),
+                    EventCard(
+                      title: 'Denices workshop on Angular',
+                      time: 'Thursday at 7:30PM',
+                      description: 'Come learn angular with SHPE!',
+                      onTap:
+                          () => showEventDialog(
+                            context,
+                            'Angular!!',
+                            'Thursday at 7:30PM',
+                            'Come learn angular with SHPE!',
+                          ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ],
